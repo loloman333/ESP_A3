917baccf9e99bd7135716b9169f65a8169bbc2ec
Killer Lorenz <killer@student.tugraz.at>
2021-01-04 12:28:50 +0100

    maost things work some dont

--- a3.c
+++ a3.c
@@ -10,16 +10,47 @@
 //-----------------------------------------------------------------------------
 //
 
+
+
+///TODO WHY is test 9 not doing anything?????
+
+
+
+
+
+
+
+
 // Includes
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 #include "framework.h"
 
 // Defines
 #define MAGIC_NUMBER "ESPipes"
 #define DO_RESTART 2
+#define HIGHSCORE_NAME_LENGTH 3
+
+#define MAX_UNIT8_T 0xFF
+#define MIN_UNIT8_T 0x00
+#define FILTER_RIGHT 0x03
+#define FILTER_LEFT 0xC0
+#define SWITCH 0x40
+
+//TODO REMOVE
+#define BYTE_TO_BINARY_PATTERN "%c%c|%c%c|%c%c|%c%c"
+#define BYTE_TO_BINARY(byte)  \
+  (byte & 0x80 ? '1' : '0'), \
+  (byte & 0x40 ? '1' : '0'), \
+  (byte & 0x20 ? '1' : '0'), \
+  (byte & 0x10 ? '1' : '0'), \
+  (byte & 0x08 ? '1' : '0'), \
+  (byte & 0x04 ? '1' : '0'), \
+  (byte & 0x02 ? '1' : '0'), \
+  (byte & 0x01 ? '1' : '0') 
 
 // Typedefs
 typedef enum _ReturnValue_
@@ -43,7 +74,7 @@ typedef struct _Board_
 typedef struct _HighscoreEntry_
 {
   uint8_t score;
-  char name[3];
+  char name[4];
 } HighscoreEntry;
 
 typedef struct _Highscore_
@@ -61,17 +92,29 @@ typedef enum _Direction_
 } Direction;
 
 // Forward Definitions     TODO: order these the same way as below
-
 ReturnValue loadGame(Board** game_board, Highscore** highscore_list, char* file_name, char** error_context);
 FILE* openConfigFile(char* file_name, ReturnValue* error_code);
 void loadConfigFile(Board** game_board, Highscore** highscore_list, FILE* file, ReturnValue* error_code);
 void loadHighscoreList(Highscore* highscore_list, FILE* file, ReturnValue* error_code);
 void loadGameBoard(Board* game_board, FILE* file, ReturnValue* error_code);
 
-ReturnValue runGame(Board* game_board, char* restart);
+ReturnValue runGame(Board* game_board, int* score, char* restart);
 Command getInput(char round, uint8_t* row, uint8_t* col, Direction* dir);
-void runCommand(Command command, Board* game_board, uint8_t row, uint8_t col, Direction dir, char* stop);
-void rotatePipe(Board* game_board, uint8_t row, uint8_t col, Direction dir);
+char runCommand(Command command, Board* game_board, uint8_t row, uint8_t col, Direction dir, char* stop);
+char rotatePipe(Board* game_board, uint8_t row, uint8_t col, Direction dir);
+void setConnectedBits(Board* game_board, uint8_t row, uint8_t col);
+void setConnectedBitInDirection(Board* game_board, uint8_t row, uint8_t col, Direction dir);
+char checkConnection(Board* game_board, uint8_t row, uint8_t col, Direction dir);
+
+void handleScore(Highscore* highscore_list, int score);
+char doesScoreBeatHighscore(Highscore* highscore_list, int score);
+char* beatHighscore();
+void printHighscore(Highscore* highscore_list);
+
+void moveCoordiantesInDirection(uint8_t* row, uint8_t* col, Direction dir);
+char areCoordinatesOnBoard(Board* game_board, uint8_t row, uint8_t col);
+Direction getOppositeDirection(Direction dir);
+char isPipeOpenInDirection(uint8_t pipe, Direction dir);
 
 void freeResources(Board* game_board, Highscore* highscore_list);
 int exitApplication(ReturnValue error_code, char* error_context);
@@ -84,7 +127,7 @@ int exitApplication(ReturnValue error_code, char* error_context);
 ///
 /// @return always zero
 //
-int main(int argc, char** argv) //TODO: char** argv == char * argv[]  ???
+int main(int argc, char** argv)
 {
   if (argc != 2)
   {
@@ -96,6 +139,7 @@ int main(int argc, char** argv) //TODO: char** argv == char * argv[]  ???
   Board* game_board = NULL;
   Highscore* highscore_list = NULL;
   char restart = false;
+  int score = 0;
 
   do 
   {
@@ -105,9 +149,14 @@ int main(int argc, char** argv) //TODO: char** argv == char * argv[]  ???
       break;
     }
     
-    error_code = runGame(game_board, &restart);
+    error_code = runGame(game_board, &score, &restart);
   }
   while (restart);
+
+  if (error_code == SUCCESS)
+  {
+    handleScore(highscore_list, score);
+  }
 
   freeResources(game_board, highscore_list);
   return exitApplication(error_code, error_context);
@@ -227,6 +276,7 @@ void loadHighscoreList(Highscore* highscore_list, FILE* file, ReturnValue* error
   {
     fread(&((highscore_list->entries[i]).score), 1, 1, file);
     fread(&((highscore_list->entries[i]).name), 3, 1, file);
+    highscore_list->entries[i].name[3] = '\0';
   }
 }
 
@@ -269,7 +319,7 @@ void loadGameBoard(Board* game_board, FILE* file, ReturnValue* error_code)
 ///
 /// @return TODO
 //
-ReturnValue runGame(Board* game_board, char* restart)
+ReturnValue runGame(Board* game_board, int* score, char* restart)
 {
   Command command = 0;
   Direction dir = 0;
@@ -277,16 +327,24 @@ ReturnValue runGame(Board* game_board, char* restart)
   uint8_t col = 0;
   char stop = false;
   char round = 1;
+  char skipPrinting = 0;
 
   while(!stop)
   {
-    printMap(
-      game_board->map, 
-      game_board->map_width, 
-      game_board->map_height, 
-      game_board->start, 
-      game_board->end
-    );
+    if (skipPrinting)
+    {
+      skipPrinting = false;
+    }
+    else
+    {
+      printMap(
+        game_board->map, 
+        game_board->map_width, 
+        game_board->map_height, 
+        game_board->start, 
+        game_board->end
+      );
+    } 
 
     command = getInput(round, &row, &col, &dir);
     if (command == NONE)
@@ -294,9 +352,15 @@ ReturnValue runGame(Board* game_board, char* restart)
       return OUT_OF_MEMORY;
     }
 
-    runCommand(command, game_board, row, col, dir, &stop);
-    round++;
-
+    if (runCommand(command, game_board, row, col, dir, &stop))
+    {
+      round++;
+    }
+    else
+    {
+      skipPrinting = true;
+    }
+    
     if (stop == DO_RESTART)
     {
       *restart = true;
@@ -315,6 +379,14 @@ ReturnValue runGame(Board* game_board, char* restart)
     }  
   }
 
+  printMap(
+    game_board->map, 
+    game_board->map_width, 
+    game_board->map_height, 
+    game_board->start, 
+    game_board->end
+  );
+  *score = round - 1;
   return SUCCESS;
 }
 
@@ -337,21 +409,20 @@ Command getInput(char round, uint8_t* row, uint8_t* col, Direction* dir)
   } 
   else if (input == (char*) EOF)
   {
-    round++;
     return QUIT;
   }
 
   Command command = NONE;
-  char* ret;
+  char* ret = parseCommand(input, &command, (size_t*)dir, row, col);
 
-  ret = parseCommand(input, &command, (size_t*)dir, row, col);
-  free(input);
+  (*row)--;
+  (*col)--;
 
   if (ret == NULL)
   {
     if (command != NONE)
     {
-      round++;
+      free(input);
       return command;
     }
   } 
@@ -364,6 +435,7 @@ Command getInput(char round, uint8_t* row, uint8_t* col, Direction* dir)
     printf(ERROR_UNKNOWN_COMMAND, ret);
   }
 
+  free(input);
   return getInput(round, row, col, dir);
 }
 
@@ -374,7 +446,7 @@ Command getInput(char round, uint8_t* row, uint8_t* col, Direction* dir)
 ///
 /// @return TODO
 //
-void runCommand(Command command, Board* game_board, uint8_t row, uint8_t col, Direction dir, char* stop)
+char runCommand(Command command, Board* game_board, uint8_t row, uint8_t col, Direction dir, char* stop)
 {
   switch (command)
   {
@@ -391,10 +463,293 @@ void runCommand(Command command, Board* game_board, uint8_t row, uint8_t col, Di
     break;
 
   case ROTATE:
-    rotatePipe(game_board, row, col, dir);
+    return rotatePipe(game_board, row, col, dir);
     break;
   
   default:
+    break;
+  }
+
+  return true;
+}
+
+//-----------------------------------------------------------------------------
+/// 
+/// TODO
+/// 
+///
+/// @return TODO
+//
+char rotatePipe(Board* game_board, uint8_t row, uint8_t col, Direction dir)
+{
+  if (! areCoordinatesOnBoard(game_board, row, col))
+  {
+    printf(USAGE_COMMAND_ROTATE);
+    return false;
+  }
+  
+
+  if ((row == game_board->start[0] && col == game_board->start[1])
+    || (row == game_board->end[0] && col == game_board->end[1]))
+  {
+    printf(ERROR_ROTATE_INVALID);
+    return false;
+  }
+
+  uint8_t new_pipe = game_board->map[row][col];
+
+  if (dir == LEFT)
+  {
+    new_pipe = ((new_pipe & FILTER_RIGHT) << 6) | (new_pipe >> 2);
+  } 
+  else if (dir == RIGHT)
+  {
+    new_pipe = ((new_pipe & FILTER_LEFT) >> 6) | (new_pipe << 2);
+  } 
+
+  game_board->map[row][col] = new_pipe;
+
+  setConnectedBits(game_board, row, col);
+
+  return true;
+}
+
+//-----------------------------------------------------------------------------
+/// 
+/// TODO
+/// 
+///
+/// @return TODO
+//
+void setConnectedBits(Board* game_board, uint8_t row, uint8_t col)
+{
+  for (Direction dir = TOP; (int) dir <= RIGHT; dir++)
+  {  
+    setConnectedBitInDirection(game_board, row, col, dir);
+
+    uint8_t new_row = row;
+    uint8_t new_col = col;
+    moveCoordiantesInDirection(&new_row, &new_col, dir);
+    if (areCoordinatesOnBoard(game_board, new_row, new_col))
+    {
+      setConnectedBitInDirection(game_board, new_row, new_col, getOppositeDirection(dir));
+    }
+  }
+}
+
+//-----------------------------------------------------------------------------
+/// 
+/// TODO
+/// 
+///
+/// @return TODO
+//
+void setConnectedBitInDirection(Board* game_board, uint8_t row, uint8_t col, Direction dir)
+{
+  uint8_t new_pipe = game_board->map[row][col];
+ 
+  char connected;
+  if (isPipeOpenInDirection(game_board->map[row][col], dir))
+  {
+    connected = checkConnection(game_board, row, col, dir);
+  }
+  else
+  {
+    connected = false;
+  }    
+
+  char shift_value = (2 * dir);
+
+  if (connected)
+  {  
+    new_pipe = new_pipe | (SWITCH >> shift_value);
+  }
+  else
+  {
+    new_pipe = new_pipe & (~(SWITCH >> shift_value));
+  }
+
+  game_board->map[row][col] = new_pipe; 
+
+}
+
+//-----------------------------------------------------------------------------
+/// 
+/// TODO
+/// 
+///
+/// @return TODO
+//
+char checkConnection(Board* game_board, uint8_t row, uint8_t col, Direction dir)
+{
+
+  moveCoordiantesInDirection(&row, &col, dir);
+
+  if (! areCoordinatesOnBoard(game_board, row, col))
+  {
+    return false;
+  }
+
+  Direction opp_dir = getOppositeDirection(dir);
+  if (isPipeOpenInDirection(game_board->map[row][col], opp_dir))
+  {
+    return true;
+  }
+
+  return false;
+}
+
+//-----------------------------------------------------------------------------
+/// 
+/// TODO
+/// 
+///
+/// @return TODO
+//
+void handleScore(Highscore* highscore_list, int score)
+{
+  printf(INFO_PUZZLE_SOLVED);
+  printf(INFO_SCORE, score);
+
+  if (doesScoreBeatHighscore(highscore_list, score))
+  {
+    char* name = beatHighscore();
+    HighscoreEntry new_entry;
+    new_entry.score = score;
+    strcpy(new_entry.name, name);
+    free(name);
+
+    for (int i = 0; i < highscore_list->count; i++)
+    {
+      if (highscore_list->entries[i].score > new_entry.score)
+      {
+        if (highscore_list->entries[i].score == 0)
+        {
+          highscore_list->entries[i] = new_entry;
+          return;
+        }
+
+        HighscoreEntry tmp = highscore_list->entries[i];
+        highscore_list->entries[i] = new_entry;
+        new_entry = tmp;
+      }
+    }
+  }
+
+  printHighscore(highscore_list);   
+}
+
+//-----------------------------------------------------------------------------
+/// 
+/// TODO
+/// 
+///
+/// @return TODO
+//
+char doesScoreBeatHighscore(Highscore* highscore_list, int score)
+{
+  for (int i = 0; i < highscore_list->count; i++)
+  {
+    if (highscore_list->entries[i].score > score)
+    {
+      return true;
+    }
+  }
+  return false;
+}
+
+//-----------------------------------------------------------------------------
+/// 
+/// TODO
+/// 
+///
+/// @return TODO
+//
+char* beatHighscore()
+{
+  printf(INFO_BEAT_HIGHSCORE);
+  printf(INPUT_NAME);
+
+  char* name;
+  char name_valid = false;
+
+  while (!name_valid)
+  {
+    name = getLine();
+
+    if (name == NULL)
+    {
+      printf(ERROR_OUT_OF_MEMORY);
+      exit(OUT_OF_MEMORY);
+    } 
+    else if (name == (char*) EOF)
+    {
+      continue;
+    }
+    else if (strlen(name) == HIGHSCORE_NAME_LENGTH)
+    {
+      name_valid = true;
+      for (int i = 0; i < HIGHSCORE_NAME_LENGTH; i++)
+      {
+        name[i] = toupper(name[i]);
+        if (name[i] > 90 || name[i] < 65)  
+        {
+          name_valid = false;
+          break;
+        }
+      }
+    }
+    if (!name_valid)
+    {
+      free(name);
+    }
+  }
+
+  return name;
+}
+
+//-----------------------------------------------------------------------------
+/// 
+/// TODO
+/// 
+///
+/// @return TODO
+//
+void printHighscore(Highscore* highscore_list)
+{
+  printf(INFO_HIGHSCORE_HEADER);
+
+  for (int i = 0; i < highscore_list->count; i++)
+  {
+    //printf("NAME: %s\n", highscore_list->entries[i].name);
+    //printf("SCORE: %d\n", highscore_list->entries[i].score);
+
+    printf(INFO_HIGHSCORE_ENTRY, highscore_list->entries[i].name, highscore_list->entries[i].score);
+  }
+}
+
+//-----------------------------------------------------------------------------
+/// 
+/// TODO
+/// 
+///
+/// @return TODO
+//
+void moveCoordiantesInDirection(uint8_t* row, uint8_t* col, Direction dir)
+{
+  switch (dir)
+  {
+  case TOP:
+    (*row)--;
+    break;
+  case LEFT:
+    (*col)--;;
+    break;
+  case BOTTOM:
+    (*row)++;
+    break;
+  case RIGHT:
+    (*col)++;
     break;
   }
 }
@@ -406,39 +761,48 @@ void runCommand(Command command, Board* game_board, uint8_t row, uint8_t col, Di
 ///
 /// @return TODO
 //
-void rotatePipe(Board* game_board, uint8_t row, uint8_t col, Direction dir)
+char areCoordinatesOnBoard(Board* game_board, uint8_t row, uint8_t col)
 {
-  if (row > game_board->map_height || col > game_board->map_width)
+  char res = (
+    row > game_board->map_height - 1 || 
+    col > game_board->map_width - 1 ||
+    row < 0 || col < 0);
+
+  return !res;
+}
+
+//-----------------------------------------------------------------------------
+/// 
+/// TODO
+/// 
+///
+/// @return TODO
+//
+Direction getOppositeDirection(Direction dir)
+{
+  for (unsigned i = 2; i > 0; i--)
   {
-    printf(USAGE_COMMAND_ROTATE);
-    return;
+    dir--;
+    if ((int) dir < TOP)
+    {
+      dir = RIGHT;
+    }
   }
+  return dir;
+}
 
-  if ((row - 1 == game_board->start[0] && col - 1 == game_board->start[1])
-    || (row - 1 == game_board->end[0] && col - 1 == game_board->end[1]))
-  {
-    printf(ERROR_ROTATE_INVALID);
-    return;
-  }
-
-  uint8_t new_pipe = game_board->map[row - 1][col - 1];
-
-  if (dir == LEFT)
-  {
-    uint8_t right = 0x03;
-    new_pipe = ((new_pipe & right) << 6) | (new_pipe >> 2);
-  } 
-  else if (dir == RIGHT)
-  {
-
-    uint8_t left = 0xC0;
-    new_pipe = ((new_pipe & left) >> 6) | (new_pipe << 2);
-  } 
-
-  //STOPPED HERE 
-  //TODO: set connected bits
-
-  game_board->map[row - 1][col - 1] = new_pipe;
+//-----------------------------------------------------------------------------
+/// 
+/// TODO
+/// 
+///
+/// @return TODO
+//
+char isPipeOpenInDirection(uint8_t pipe, Direction dir)
+{
+  uint8_t checker = 0x80;
+  
+  return pipe & (checker >> (2 * dir));
 }
 
 //-----------------------------------------------------------------------------
